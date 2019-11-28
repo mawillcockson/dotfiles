f5e66a27dc4690a27d4629e9f32405e005b95669
Matthew Willcockson <matthew@willcockson.family>
2019-11-27 23:26:35 -0600

    What should invoke be used for?
    I think it'd be good to have invoke be used for only bootstrapping and
    very complex tasks.
    
    A lot of the tools I want to have installed, like plug.vim or oh-my-zsh,
    already have curl/sh oneliners in their READMEs for easy installation.
    
    These fit perfectly into dotdrop actions.
    
    The invoke tool can be used to bootstrap the installation of those
    tools, and to perform more complex tasks and installations.
    
    Also, it is kind of cool that, since pipx uses pip, it can install from
    anywhere pip can, like the git+https:// urls, or a local directory.
    
    So I could put this tool in its own package in the repository, and have
    it install itself with pipx, and make itself available everywhere.
    
    Alternatively, if I can put it all in one file, I think that'd be much
    better, as the tool would really only need to be used for this
    repository, and hopefully won't be too complicated or long.
    
    It seems to me like
    setup.py --install=all --profile=""
    would be an ideal thing for install.py to be able to run, to install all
    the required tools used by dotdrop actions, and to start building a new
    profile for the device we're running the command from.

--- tasks.py
+++ tasks.py
@@ -22,3 +22,18 @@ def setup_gpg(ctx, clean=False):
 #git clone git@github.com:mawillcockson/dotfiles.git projects/dotfiles
 #alias dotdrop='dotdrop --cfg=~/projects/dotfiles/config.yaml'
 #dotdrop install
+
+"""
+The above is a good example of what this utility should be about, except for setting aliases.
+
+This utility should not
+# Install Oh-My-Zsh: https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+# Install plug.vim as per dotdrop docs
+
+That's best left up to dotdrop actions.
+
+This utility should do the things to get dotdrop ready, and to get the other utilities in place that dotdrop will call.
+It can download/install cURL, git, scoop, PowerShell Core, etc. The utilities required by dotdrop actions to bootstrap install.
+It can also have a profile subcommand that is able to create a new dotdrop profile based on what functionality/features
+are desired.
+"""
