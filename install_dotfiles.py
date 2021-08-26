9f0fdf395bbe7a4ce7d9064ec1e704288f7c7b6f
Matthew Willcockson <matthew@willcockson.family>
2021-08-26 04:44:20 -0500

    feat: found a bug

--- install_dotfiles.py
+++ install_dotfiles.py
@@ -15,6 +15,7 @@ Steps:
 """
 import os
 import sys
+import time
 from pathlib import Path
 from shutil import which
 from subprocess import (
@@ -27,10 +28,15 @@ from subprocess import (
     run,
 )
 from tempfile import TemporaryDirectory
-from time import sleep
-from typing import Dict, List, Optional
+from typing import TYPE_CHECKING, Dict, List, Optional, Union
 from unittest.mock import patch
 from urllib.request import urlopen
+
+trio = None
+
+if TYPE_CHECKING:
+    import trio
+    from trio import Process
 
 WINDOWS = sys.platform.startswith(("win", "cygwin")) or (
     sys.platform == "cli" and os.name == "nt"
@@ -43,7 +49,20 @@ if WINDOWS:
     import winreg
 
 
-class Installer:
+def get_user_env(name: str) -> Optional[str]:
+    if not WINDOWS:
+        raise NotImplementedError(
+            "can only update environment variables on Windows for now"
+        )
+
+    with winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER) as root:
+        with winreg.OpenKey(root, "Environment", 0, winreg.KEY_ALL_ACCESS) as key:
+            value, _ = winreg.QueryValueEx(key, name)
+
+            return value
+
+
+class Bootstrapper:
     UPDATED_ENVIRONMENT: Dict[str, str] = {}
     SHELL: Optional[Path] = None
     _SCOOP_INSTALLED = False
@@ -82,18 +101,6 @@ class Installer:
             self.cmd([self.PYTHON_EXECUTABLE, "-m", "pip", "--version"]).returncode == 0
         )
         self._PIP_INSTALLED = False
-
-    def get_user_env(self, name: str) -> Optional[str]:
-        if not WINDOWS:
-            raise NotImplementedError(
-                "can only update environment variables on Windows for now"
-            )
-
-        with winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER) as root:
-            with winreg.OpenKey(root, "Environment", 0, winreg.KEY_ALL_ACCESS) as key:
-                value, _ = winreg.QueryValueEx(key, name)
-
-                return value
 
     def cmd(self, args: List[str], stdin: str = "") -> CompletedProcess:
         print(f"running -> {args!r}")
@@ -142,20 +149,13 @@ class Installer:
                 executable=str(self.SHELL) or None,
             )
 
-        print(f"{result.stdout or ''}\n{result.stderr or ''}")
+        if result.stdout:
+            print(result.stdout)
+        if result.stderr:
+            print(result.stderr)
         return result
 
-    def scoop(args: str) -> CompletedProcess:
-        if not (WINDOWS and self._SCOOP_INSTALLED):
-            raise Exception(
-                "not running scoop when not on Windows or scoop not installed"
-            )
-
-        result = self.shell(f"scoop {args}")
-        result.check_returncode()
-        return result
-
-    def bootstrap_async(self) -> None:
+    def main(self) -> None:
         try:
             import virtualenv
         except ImportError:
@@ -182,9 +182,20 @@ class Installer:
 
         # Install trio
         self.pip(["install", "trio"])
-        import trio  # isort:skip
+        import trio as trio_module # isort:skip
+        global trio
+        trio = trio_module
 
-        self.main()
+        installer = Installer(
+            temp_dir=self.TEMP_DIR,
+            repository_dir=self.REPOSITORY_DIR,
+            shell=self.SHELL,
+            venv_dir=self.VENV_DIR,
+            cache_dir=self.CACHE_DIR,
+            python_executable=self.PYTHON_EXECUTABLE,
+            updated_environment=self.UPDATED_ENVIRONMENT,
+        )
+        trio.run(installer.main)
 
     def bootstrap_virtualenv(self) -> None:
         if not self._PIP_INSTALLED:
@@ -229,6 +240,7 @@ class Installer:
             ]
         )
         sys.path.insert(0, str(self.PIP_DIR))
+        # Causes Python to find the downloaded pip module
         self.UPDATED_ENVIRONMENT["PYTHONPATH"] = str(self.PIP_DIR)
         self._PIP_INSTALLED = True
 
@@ -240,44 +252,128 @@ class Installer:
         # https://stackoverflow.com/q/63783587
         self.cmd([self.PYTHON_EXECUTABLE, "-m", "pip", *args, "--no-user"])
 
-    def install_scoop(self) -> None:
+
+class Installer:
+    SHELL: Optional[Path] = None
+    PYTHON_EXECUTABLE: str = sys.executable
+    UPDATED_ENVIRONMENT: Dict[str, str] = {}
+    _SCOOP_INSTALLED: bool = False
+    PROCESS_TYPES: Dict[str, str] = {
+        "cmd": "{0!r}",
+        "shell": '"{0}"',
+        "pip": "{0}",
+        "scoop": "{0}",
+    }
+
+    def __init__(
+        self,
+        temp_dir: Path,
+        repository_dir: Path,
+        shell: Optional[Path] = None,
+        venv_dir: Optional[Path] = None,
+        cache_dir: Optional[Path] = None,
+        python_executable: str = sys.executable,
+        updated_environment: Dict[str, str] = {},
+    ) -> None:
+        if WINDOWS:
+            if not shell:
+                powershell_str = which("powershell")
+                powershell_path = Path(powershell_str).resolve()
+                if not (powershell_str and powershell_path.is_file()):
+                    raise FileNotFoundError(
+                        f"powershell not found at '{powershell_str}' or '{powershell_path}'"
+                    )
+                self.SHELL = powershell_path
+
+            else:
+                self.SHELL = shell
+
+        self.REPOSITORY_DIR = repository_dir
+        self.TEMP_DIR = temp_dir
+        assert self.TEMP_DIR.is_dir()
+        self.VENV_DIR = venv_dir or (self.TEMP_DIR / "venv")
+        self.VENV_DIR.mkdir(exist_ok=True)
+        self.CACHE_DIR = cache_dir or (self.TEMP_DIR / "cache")
+        self.CACHE_DIR.mkdir(exist_ok=True)
+        self.PYTHON_EXECUTABLE = python_executable
+        self.UPDATED_ENVIRONMENT.update(updated_environment)
+
+    async def scoop(args: str) -> CompletedProcess:
+        if not (WINDOWS and self._SCOOP_INSTALLED):
+            raise Exception(
+                "not running scoop when not on Windows or scoop not installed"
+            )
+
+        return await self.shell(f"scoop {args}", check=True, process_type="scoop")
+
+    async def cmd(
+        self,
+        args: Union[str, List[str]],
+        check: bool = True,
+        shell: bool = False,
+        process_type: str = "cmd",
+    ) -> CompletedProcess:
+        args_str = self.PROCESS_TYPES.get(
+            process_type, self.PROCESS_TYPES["cmd"]
+        ).format(args)
+        if shell:
+            assert isinstance(args, str), "args must be a string of code for the shell"
+
+        cmd_str = f"{process_type} -> {args_str}"
+        print(cmd_str)
+
+        # Trio magically copies stdout and stderr streams to sys.stdout and sys.stderr!
+        return await trio.run_process(
+            args,
+            stdin=None,
+            shell=shell,
+            executable=str(self.SHELL) or None if shell else None,
+            env=self.UPDATED_ENVIRONMENT or None,
+            check=check,
+        )
+
+    async def shell(self, code: str, check: bool = True) -> CompletedProcess:
+        return await self.cmd(code, check=check, shell=True, process_type="shell")
+
+    async def install_scoop(self) -> None:
         if not WINDOWS:
             raise Exception("not installing scoop when not on Windows")
 
         # Check if scoop is already installed
-        self.UPDATED_ENVIRONMENT["PATH"] = self.get_user_env("PATH")
+        self.UPDATED_ENVIRONMENT["PATH"] = get_user_env("PATH")
 
-        result = self.shell("scoop which scoop")
-        print(f"returncode -> {result.returncode}")
+        process = await self.shell("scoop which scoop", check=False)
         error_msg = "is not recognized as the name of"
-        if (
-            error_msg in result.stdout
-            or error_msg in result.stderr
-            or result.returncode != 0
-        ):
+        if error_msg in process.stdout.decode() or process.returncode != 0:
             # Set PowerShell's Execution Policy
             args = [
-                str(SHELL),
+                str(self.SHELL),
                 "-c",
                 "& {Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser}",
             ]
             print(f"running -> {args!r}")
 
-            set_executionpolicy = Popen(args, text=True)
-            print("waiting...")
-            sleep(2)
-            try:
-                stdout, stderr = set_executionpolicy.communicate("A", timeout=2)
-            except TimeoutExpired:
-                set_executionpolicy.kill()
-                stdout, stderr = set_executionpolicy.communicate()
+            async with await trio.open_process(
+                args, stdin=PIPE, stdout=PIPE, stderr=STDOUT, check=False, timeout=2
+            ) as set_executionpolicy:
+                # Wait for warning message or process completion or timeout
+                data: bytes = await set_executionpolicy.stdio.receive_some()
+                expect = '(default is "N"):'.encode()
+                while expect not in data or set_executionpolicy.returncode == None:
+                    data += await set_executionpolicy.stdio.receive_some()
 
-            print(f"{stdout or ''}\n{stderr or ''}")
+                if expect not in data and set_executionpolicy.returncode != None:
+                    raise Exception("Set-ExecutionPolicy message never received")
 
-            print("waiting again...")
-            sleep(2)
+                # respond to warning message with encoded "A"
+                await set_executionpolicy.stdio.send_all("A".encode())
 
-            result = cmd([str(SHELL), "-c", "& {Get-ExecutionPolicy}"])
+                # wait for process completion or timeout
+                await set_executionpolicy.wait()
+
+            result = await self.cmd(
+                [str(self.SHELL), "-c", "& {Get-ExecutionPolicy}"], check=False
+            )
             if not "RemoteSigned" in result.stdout.decode():
                 raise Exception("could not set PowerShell Execution Policy")
 
@@ -291,10 +387,10 @@ class Installer:
             ):
                 raise Exception("scoop was not installed")
 
-            self.UPDATED_ENVIRONMENT["PATH"] = self.get_user_env("PATH")
+            self.UPDATED_ENVIRONMENT["PATH"] = get_user_env("PATH")
             self._SCOOP_INSTALLED = True
 
-        installed_apps = self.scoop("list").stdout
+        installed_apps = self.scoop("list").stdout.decode()
         for requirement in ["git", "aria2"]:
             if requirement in installed_apps:
                 continue
@@ -302,20 +398,22 @@ class Installer:
             self.scoop(f"install {requirement}")
 
         wanted_buckets = ["extras"]
-        added_buckets = self.scoop("bucket list").stdout
+        added_buckets = self.scoop("bucket list").stdout.decode()
         for bucket in wanted_buckets:
             if bucket in added_buckets:
                 continue
 
             self.scoop(f"bucket add {bucket}")
 
-    def main(self) -> None:
-        import trio
+    async def pip(self, args: List[str]) -> CompletedProcess:
+        return await self.cmd(
+            [self.PYTHON_EXECUTABLE, "-m", "pip", *args, "--no-user"],
+            process_type="pip",
+        )
 
-        sys.exit("Not implemented yet")
-
+    async def main(self) -> None:
         # Install dulwich
-        self.pip(["install", "dulwich"])
+        await self.pip(["install", "dulwich"])
         import dulwich  # isort:skip
 
         # Install rest of dependencies
@@ -324,40 +422,47 @@ class Installer:
 
         if WINDOWS:
             # implicitly installs git as well
-            self.install_scoop()
-            self.scoop("install python")
+            await self.install_scoop()
+            await self.scoop("install python")
 
-        for dependency_check in (["git", "--version"], ["python", "--version"]):
+        async for dependency_check in (["git", "--version"], ["python", "--version"]):
             try:
-                cmd(dependency_check).check_returncode()
+                process = await self.cmd(dependency_check, check=True)
             except CalledProcessError as err:
                 raise Exception(
                     f"dependency '{dependency_check!r}' was not found"
                 ) from err
 
-        # Clone repository
+        ## Clone dotfiles repository
         self.REPOSITORY_DIR.mkdir(parents=True, exist_ok=True)
 
-        git_status = cmd(["git", "-C", str(self.REPOSITORY_DIR), "status"])
-        if git_status.returncode != 0:
-            result = cmd(
-                [
-                    "git",
-                    "clone",
-                    "https://github.com/mawillcockson/dotfiles.git",
-                    str(self.REPOSITORY_DIR),
-                ]
-            )
-            result.check_returncode()
-
-        # Check if repository is clean
+        # Check if there's an existing repository, and if that repository is
+        # clean
         # NOTE: dulwich helpful here
         # raise Exception("dotfiles installed but perhaps there are uncommitted changes")
 
-        # Setup dotfiles
+        # Clone or pull repo
+
+        # Run dotdrop
+        print("done")
+        sys.exit(0)
         raise NotImplementedError("setup dotfiles")
 
 
 if __name__ == "__main__":
     with TemporaryDirectory() as temp_dir:
-        Installer(Path(temp_dir).resolve(strict=True)).bootstrap_async()
+        temp_dir_path = Path(temp_dir).resolve(strict=True)
+        bootstrapper = Bootstrapper(temp_dir_path)
+        bootstrapper.main()
+        # import trio  # isort:skip
+
+        # installer = Installer(
+        #     temp_dir=bootstrapper.TEMP_DIR,
+        #     repository_dir=bootstrapper.REPOSITORY_DIR,
+        #     shell=bootstrapper.SHELL,
+        #     venv_dir=bootstrapper.VENV_DIR,
+        #     cache_dir=bootstrapper.CACHE_DIR,
+        #     python_executable=bootstrapper.PYTHON_EXECUTABLE,
+        #     updated_environment=bootstrapper.UPDATED_ENVIRONMENT,
+        # )
+        # trio.run(installer.main)
--- notes.md
+++ notes.md
@@ -1,3 +1,9 @@
-# Arch/Manjaro
+# notes
+
+## Arch/Manjaro
 
 `ln -s /usr/lib/libffi-3.2.1/include/ffi*.h /usr/include`
+
+## Weird errors
+
+Consistently reproducible `Fatal Python error: _Py_HashRandomization_Init: failed to get random numbers to initialize Python` when using `pip` to install `dulwich`.
