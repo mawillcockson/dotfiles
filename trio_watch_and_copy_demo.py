a3122ad5db9bc7c96f772e1294e9b233e3b1c09f
Matthew Willcockson <matthew@willcockson.family>
2021-08-28 06:27:39 -0500

    feat(demo): reordering didn't help, using asyncconextmanager did; it works!

--- trio_watch_and_copy_demo.py
+++ trio_watch_and_copy_demo.py
@@ -3,45 +3,37 @@ import subprocess
 import sys
 import time
 from collections import deque
-from contextlib import AbstractAsyncContextManager, AsyncExitStack
+from contextlib import asynccontextmanager
 from functools import partial
 from io import BufferedWriter
 from pathlib import Path
 from shutil import which
 from subprocess import PIPE, STDOUT
-from typing import Deque, Dict, List, Tuple, Union
+from typing import AsyncIterator, Deque, Dict, List, Tuple, Union
 
 import trio
 from trio import Event, MemoryReceiveChannel, MemorySendChannel, Nursery, Process
 from trio.abc import ReceiveStream, SendStream
 
 
-class Expect(AbstractAsyncContextManager):
+class Expect:
     def __init__(
         self,
-        nursery: Nursery,
-        args: Union[str, List[str]],
-        shell: bool = False,
+        process: Process,
+        printer_send_channel: "MemorySendChannel[bytes]",
+        printer_receive_channel: "MemoryReceiveChannel[bytes]",
+        notifier_send_channel: "MemorySendChannel[bytes]",
+        opened_notifier_receive_channel: "MemoryReceiveChannel[bytes]",
         print_buffer: BufferedWriter = sys.stdout.buffer,
     ):
-        self.nursery = nursery
-        self.args = args
-        self.shell = shell
+        self.process = process
+        self.printer_send_channel = printer_send_channel
+        self.printer_receive_channel = printer_receive_channel
+        self.notifier_send_channel = notifier_send_channel
+        self.opened_notifier_receive_channel = opened_notifier_receive_channel
         self.print_buffer = print_buffer
-
-        printer_channels: "Tuple[MemorySendChannel[bytes], MemoryReceiveChannel[bytes]]" = trio.open_memory_channel(
-            1
-        )
-        self.printer_send_channel, self.printer_receive_channel = printer_channels
-        notifier_channels: "Tuple[MemorySendChannel[bytes], MemoryReceiveChannel[bytes]]" = trio.open_memory_channel(
-            0
-        )
-        self.notifier_send_channel, self.notifier_receive_channel = notifier_channels
-
-        self.process: Optional[Process] = None
         self.stdout: bytes = b""
         self.response_sent = False
-        self.exit_stack: Optional[AsyncExitStack] = None
 
     async def printer(
         self,
@@ -65,7 +57,7 @@ class Expect(AbstractAsyncContextManager):
 
         async with self.process.stdout, self.printer_send_channel, self.notifier_send_channel:
             async for chunk in self.process.stdout:
-                print(f"seen chunk: '{chunk!r}'", flush=True)
+                # print(f"seen chunk: '{chunk!r}'", flush=True)
                 self.stdout += chunk
                 await self.printer_send_channel.send(chunk)
 
@@ -83,40 +75,46 @@ class Expect(AbstractAsyncContextManager):
             raise Exception("missing process; was this called inside a with statement?")
 
         self.response_sent = False
-        async with self.notifier_receive_channel.clone() as notifier_receive_channel:
-            print("opened notifier channel", flush=True)
+        async with self.opened_notifier_receive_channel.clone() as notifier_receive_channel:
+            # print("expect --> opened notifier channel", flush=True)
             async for notification in notifier_receive_channel:
-                print("received chunk notification", flush=True)
-                if not response_sent and watch_for in self.stdout:
-                    print("sending response...", flush=True)
-                    await process.stdin.send_all(respond_with)
+                # print("expect --> received chunk notification", flush=True)
+                if not self.response_sent and watch_for in self.stdout:
+                    print("expect --> sending response...", flush=True)
+                    await self.process.stdin.send_all(respond_with)
                     self.response_sent = True
-                    print("response sent", flush=True)
+                    print("expect --> response sent", flush=True)
 
-    async def __aenter__(self) -> "Expect":
-        if self.exit_stack:
-            raise Exception("cannot reenter context manager")
-
-        # I'm pretty sure the order here is important
-        self.exit_stack = AsyncExitStack()
-        self.process = await self.exit_stack.enter_async_context(
-            await trio.open_process(
-                self.args, stdin=PIPE, stdout=PIPE, stderr=STDOUT, shell=self.shell
-            )
+    @classmethod
+    @asynccontextmanager
+    async def open_process(cls, args: Union[str, List[str]]) -> "AsyncIterator[Expect]":
+        printer_channels: "Tuple[MemorySendChannel[bytes], MemoryReceiveChannel[bytes]]" = trio.open_memory_channel(
+            1
         )
-        await self.exit_stack.enter_async_context(self.notifier_receive_channel)
+        printer_send_channel, printer_receive_channel = printer_channels
+        notifier_channels: "Tuple[MemorySendChannel[bytes], MemoryReceiveChannel[bytes]]" = trio.open_memory_channel(
+            0
+        )
+        notifier_send_channel, notifier_receive_channel = notifier_channels
 
-        self.nursery.start_soon(self.copier_recorder)
-        self.nursery.start_soon(self.printer)
+        async with notifier_receive_channel:
+            async with await trio.open_process(
+                args, stdin=PIPE, stdout=PIPE, stderr=STDOUT
+            ) as process:
+                async with trio.open_nursery() as nursery:
+                    expect = cls(
+                        process=process,
+                        printer_send_channel=printer_send_channel,
+                        printer_receive_channel=printer_receive_channel,
+                        notifier_send_channel=notifier_send_channel,
+                        opened_notifier_receive_channel=notifier_receive_channel,
+                    )
+                    nursery.start_soon(expect.copier_recorder)
+                    nursery.start_soon(expect.printer)
 
-        return self
+                    yield expect
 
-    async def __aexit__(self, exc_type, exc, tb) -> None:
-        if not self.exit_stack:
-            raise Exception("context manager never entered")
-
-        await self.exit_stack.aclose()
-        await self.process.wait()
+                    await expect.process.wait()
 
 
 async def expect_example(
@@ -132,50 +130,79 @@ async def expect_example(
 
     python_process_code = f"""
 import time
-print("process thinking...", flush=True)
-print("process waiting for {process_hello_time} seconds", flush=True)
-time.sleep({process_hello_time:f})
-print("process says hello", flush=True)
-print(f"process received: {{input()}}", flush=True)
-print("process waiting for {process_wait_time} seconds", flush=True)
-time.sleep({process_wait_time})
-print("process says thanks", flush=True)
+print("process -> waiting for {process_hello_time} seconds", flush=True)
+
+start = time.monotonic()
+i = 0
+while True:
+    print(i, end=" ", flush=True)
+    i += 1
+    elapsed_time = time.monotonic() - start
+    if elapsed_time >= {process_hello_time:f}:
+        print("", flush=True)
+        break
+    elif elapsed_time < {process_hello_time:f} - 1:
+        time.sleep(1)
+    else:
+        time_left = {process_hello_time:f} - elapsed_time
+        time.sleep(time_left if time_left > 0 else 0)
+        print("")
+        break
+
+print("process -> say hello", flush=True)
+print(f"process -> received: {{input()}}", flush=True)
+print("process -> waiting for {process_wait_time} seconds", flush=True)
+
+start = time.monotonic()
+i = 0
+while True:
+    print(i, end=" ", flush=True)
+    i += 1
+    elapsed_time = time.monotonic() - start
+    if elapsed_time >= {process_hello_time:f}:
+        print("", flush=True)
+        break
+    elif elapsed_time < {process_hello_time:f} - 1:
+        time.sleep(1)
+    else:
+        time_left = {process_hello_time:f} - elapsed_time
+        time.sleep(time_left if time_left > 0 else 0)
+        print("")
+        break
+
+print("process -> says thanks", flush=True)
 """
 
-    async with trio.open_nursery() as nursery:
-        with trio.move_on_after(total_timeout):
-            async with Expect(
-                nursery,
-                [sys.executable, "-c", python_process_code],
-            ) as expect:
+    with trio.move_on_after(total_timeout):
+        async with Expect.open_process(
+            [sys.executable, "-c", python_process_code],
+        ) as expect:
 
-                print("expect timeout started", flush=True)
-                start = time.monotonic()
+            with trio.move_on_after(expect_timeout):
                 await expect.expect(
-                    b"hello",
-                    b"hi\n",
+                    watch_for=b"hello",
+                    respond_with=b"hi\n",
                 )
 
-                print(
-                    f"expect over after {time.monotonic() - start} seconds", flush=True
-                )
+            if not expect.response_sent and expect.process.returncode == None:
+                print("expect --> expect timeout")
+                expect.process.kill()
 
-        if not expect.response_sent and expect.process.returncode == None:
-            print("total timeout", flush=True)
-            await trio.sleep(math.inf)
-            time.sleep(math.inf)
-            expect.process.kill()
+    if expect.response_sent and expect.process.returncode != 0:
+        print("expect --> total timeout")
 
-    stdout = expect.stdout.decode()
+    stdout = "\n".join(f"       --> {line}" for line in expect.stdout.decode().splitlines())
 
-    print(f"recorded stdout:\n{stdout}")
+    print(f"expect --> recorded stdout:\n{stdout}")
 
 
 if __name__ == "__main__":
-    print("regular")
+    print("## regular ##")
     trio.run(expect_example)
 
-    print("no timeout")
+    print("")
+
+    print("## no timeout ##")
     trio.run(
         partial(
             expect_example,
@@ -186,23 +213,27 @@ if __name__ == "__main__":
         )
     )
 
-    print("expect timeout")
+    print("")
+
+    print("## expect timeout ##")
     trio.run(
         partial(
             expect_example,
             process_hello_time=5.0,
-            process_total_time=10.0,
+            process_total_time=50.0,
             expect_timeout=4.9,
             total_timeout=15.0,
         )
     )
 
-    print("total timeout")
+    print("")
+
+    print("## total timeout ##")
     trio.run(
         partial(
             expect_example,
             process_hello_time=5.0,
-            process_total_time=10.0,
+            process_total_time=50.0,
             expect_timeout=7.0,
             total_timeout=9.9,
         )
