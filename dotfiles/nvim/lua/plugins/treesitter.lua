4bc4835b5f8ff9afe1e9b8ba96b539f9091827f3
Matthew W <matthew@willcockson.family>
2024-02-08 02:46:58 -0600

    nvim(treesitter): only enable on systems with a compiler

--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,48 @@
+-- DONE: make the condition based off how nvim-treesitter itself searches to
+-- see if a C compiler is installed, and only enable it when one is.
+-- Based off of nvim-tresitter instal process:
+-- https://github.com/nvim-treesitter/nvim-treesitter/blob/v0.9.2/lua/nvim-treesitter/install.lua#L19
+-- https://github.com/nvim-treesitter/nvim-treesitter/blob/v0.9.2/lua/nvim-treesitter/shell_command_selectors.lua#L74-L80
+
+local default_compilers = { vim.fn.getenv "CC", "cc", "gcc", "clang", "cl", "zig" }
+
+---@return bool|nil
+function has_compiler()
+  return vim.tbl_filter(function(c) ---@param c string
+    return c ~= vim.NIL and vim.fn.executable(c) == 1
+  end, default_compilers)[1]
+end
+
+vim.notify("has_compiler() -> " .. tostring(has_compiler()) .. "\n" ..
+           "has tree-sitter -> " .. vim.fn.executable("tree-sitter"),
+  vim.log.levels.INFO,
+  {}
+)
+
 return {
   "nvim-treesitter/nvim-treesitter",
-  -- NOTE: make the condition based off how nvim-treesitter itself seraches to
-  -- see if a C compiler is installed, and only enable it when one is
-  cond = false,
+  enabled = has_compiler() and vim.fn.executable "tree-sitter",
+  version = "*",
   build = ":TSUpdate",
   config = function() 
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
         ensure_installed = {
+          -- NOTE: need better automatic installation
+          -- NOTE: commented out languages have either broken parsers, or don't
+          -- have automatically included parsers:
+          -- https://github.com/nvim-treesitter/nvim-treesitter/tree/v0.9.2#adding-parsers
+
+          "python", "markdown", "javascript", "clojure", "html", "css", "scss",
+          --[==[
           -- commonly used
-          "lua", "python", "markdown",
+          --[["lua",]] "python", "markdown",
           -- less common
-          "toml", "html", "css", "bash", "powershell",
+          "toml", "html", "css", "bash", --[["powershell",]]
           -- uncommon/hopeful
-          "javascript", "scss", "rust", "powershell", "clojure", "csharp", "haskell",
+          "javascript", "scss", "rust", "clojure", --[["csharp",]] "haskell",
+          --]==]
         },
         sync_install = false,
         highlight = { enable = true },
