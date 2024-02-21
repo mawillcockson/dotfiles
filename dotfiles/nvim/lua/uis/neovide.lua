e4d0d6510100cbd855818e545123dd08460b6502
Matthew W <matthew@willcockson.family>
2024-02-21 17:17:06 -0600

    feat(nvim/neovide): add new settings

--- dotfiles/nvim/lua/uis/neovide.lua
+++ dotfiles/nvim/lua/uis/neovide.lua
@@ -1,8 +1,13 @@
 vim.notify("ran neovide", vim.log.levels.DEBUG, {})
 -- https://github.com/neovide/neovide/wiki/Configuration
 vim.g.neovide_refresh_rate = 60
+vim.g.neovide_refresh_rate_idle = 5
 vim.g.neovide_cursor_animation_length = 0
+-- vim.g.neovide_scroll_animation_far_lines = 0
 vim.g.neovide_input_use_logo = true
+vim.g.neovide_theme = "auto"
+vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_remember_window_size = true
 
 local ok, fonts = pcall(require, "uis.fonts")
 if not ok then
