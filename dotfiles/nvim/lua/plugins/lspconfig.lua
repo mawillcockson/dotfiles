3b609fec43af8b97554995ebe02ff343e5246311
Matthew W <matthew@willcockson.family>
2024-03-18 23:14:33 -0500

    feat(nvim/lsp): update keymappings

--- dotfiles/nvim/lua/plugins/lspconfig.lua
+++ dotfiles/nvim/lua/plugins/lspconfig.lua
@@ -15,10 +15,10 @@ return {
       },
     }
 
-		vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+		vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 		vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-		vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+		vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 		-- Use LspAttach autocommand to only map the following keys after the
 		-- language server attaches to the current buffer
     local lsp_group = vim.api.nvim_create_augroup("UserLspConfig", {})
@@ -36,18 +36,18 @@ return {
 				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 				-- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
 				vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-				-- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-				-- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-				-- vim.keymap.set('n', '<space>wl', function()
+				-- vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+				-- vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+				-- vim.keymap.set('n', '<leader>wl', function()
 				--   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 				-- end, opts)
-				vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-				vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-				-- vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+				vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
 				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 				-- conform.nvim will handle formatting, falling back to the lsp
 				-- optionally
-				-- vim.keymap.set('n', '<space>f', function()
+				-- vim.keymap.set('n', '<leader>f', function()
 				--   vim.lsp.buf.format { async = true }
 				-- end, opts)
 			end,
