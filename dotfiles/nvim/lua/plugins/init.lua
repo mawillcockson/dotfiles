ee172b6f9feadcd0f42b25e48b0c660eba3dc7bc
Matthew W <matthew@willcockson.family>
2024-03-26 05:13:28 -0500

    feat(nvim): configure lua language server

--- dotfiles/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/lua/plugins/init.lua
@@ -3,40 +3,54 @@
 -- NOTE: all plugins should have a version = "*" or config.defaults.version = "*" to be set
 -- https://github.com/folke/lazy.nvim#versioning
 return {
-  {
-    "folke/lazy.nvim",
-    version = "*",
-    branch = "main",
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1001,
-    version = "*",
-    branch = "main",
-    opts = {
-      flavour = "latte",
-      background = {
-        light = "latte",
-        dark = "mocha",
-      },
-      term_colors = true,
-    },
-    config = function(opts)
-      -- vim.g.catppuccin_debug = true
-      require("catppuccin").setup(opts.opts)
-      vim.cmd.colorscheme "catppuccin"
-    end,
-  },
-  {
-    "onsails/lspkind.nvim",
-    config = function(_, _)
-      require("lspkind").init{
-        mode = "symbol_text",
-      }
-    end,
-  },
-  "nvim-lua/plenary.nvim",
-  "tpope/vim-surround",
-  "tpope/vim-repeat",
+	{
+		"folke/lazy.nvim",
+		version = "*",
+		branch = "main",
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		version = "*",
+		lazy = false,
+		priority = 99, -- https://github.com/williamboman/mason-lspconfig.nvim/tree/v1.27.0#setup
+		config = true,
+	},
+	{
+		"williamboman/mason.nvim",
+		version = "*",
+		lazy = false,
+		priority = 100, -- https://github.com/williamboman/mason-lspconfig.nvim/tree/v1.27.0#setup
+		config = true,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1001,
+		version = "*",
+		branch = "main",
+		opts = {
+			flavour = "latte",
+			background = {
+				light = "latte",
+				dark = "mocha",
+			},
+			term_colors = true,
+		},
+		config = function(opts)
+			-- vim.g.catppuccin_debug = true
+			require("catppuccin").setup(opts.opts)
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		"onsails/lspkind.nvim",
+		config = function(_, _)
+			require("lspkind").init({
+				mode = "symbol_text",
+			})
+		end,
+	},
+	"nvim-lua/plenary.nvim",
+	"tpope/vim-surround",
+	"tpope/vim-repeat",
 }
--- dotfiles/nvim/lua/plugins/lspconfig.lua
+++ dotfiles/nvim/lua/plugins/lspconfig.lua
@@ -1,56 +1,114 @@
 return {
-	-- https://github.com/neovim/nvim-lspconfig?tab=readme-ov-file#suggested-configuration
-	"neovim/nvim-lspconfig",
-	-- lazy = true,
-	config = function(name, opts)
-		local lspconfig = require("lspconfig")
-		-- not needed, as there's nothing in the base project to configure
-		-- lspconfig.config(name, opts)
-		lspconfig.ruff_lsp.setup{
-      init_options = {
-        settings = {
-          -- And extra CLI arguments for ruff
-          args = {},
-        },
-      },
-    }
+	{
+		"LuaCATS/love2d",
+	},
+	{
+		-- https://github.com/neovim/nvim-lspconfig?tab=readme-ov-file#suggested-configuration
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		priority = 98, -- https://github.com/williamboman/mason-lspconfig.nvim/tree/v1.27.0#setup
+		dependencies = {
+			-- lua_ls
+			"LuaCATS/love2d",
+		},
+		config = function(name, opts)
+      -- NOTE::DIRECTION
+      -- I would like for this to autoconfigure available clients on an as-needed basis.
+      -- I would also like for no LSPs to be configured or started unless a
+      -- command or key-combination (if the latter, preferably with a prompt)
+      -- is entered.
+      -- I think it's also a good idea to do the same when configuring the
+      -- completion plugin: by default, it's inactive, and only when a
+      -- key-combination is pressed, or a command is run, is it activated.
+      local lspconfig = require("lspconfig")
+			-- not needed, as there's nothing in the base project to configure
+			-- lspconfig.config(name, opts)
+			lspconfig.ruff_lsp.setup({
+				init_options = {
+					settings = {
+						-- And extra CLI arguments for ruff
+						args = {},
+					},
+				},
+			})
 
-		vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
-		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-		vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-		vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
-		-- Use LspAttach autocommand to only map the following keys after the
-		-- language server attaches to the current buffer
-    local lsp_group = vim.api.nvim_create_augroup("UserLspConfig", {})
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = lsp_group,
-			callback = function(ev)
-				-- Enable completion triggered by <c-x><c-o>
-				vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+			lspconfig.lua_ls.setup({
+				on_init = function(client)
+					local is_nvim = false
+					for _, workspace_folder in ipairs(client.workspace_folders) do
+						if workspace_folder.name:find("dotfiles/nvim", 1, true) then
+							is_nvim = true
+							break
+						end
+					end
+					if not is_nvim then
+						return
+					end
 
-				-- Buffer local mappings.
-				-- See `:help vim.lsp.*` for documentation on any of the below functions
-				local opts = { buffer = ev.buf }
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-				-- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-				vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-				-- vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-				-- vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-				-- vim.keymap.set('n', '<leader>wl', function()
-				--   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-				-- end, opts)
-				vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-				vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-				-- conform.nvim will handle formatting, falling back to the lsp
-				-- optionally
-				-- vim.keymap.set('n', '<leader>f', function()
-				--   vim.lsp.buf.format { async = true }
-				-- end, opts)
-			end,
-		})
-	end,
+					-- https://github.com/neovim/nvim-lspconfig/blob/6e5c78ebc9936ca74add66bda22c566f951b6ee5/doc/server_configurations.md?plain=1#L6275-L6300
+					client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+						runtime = {
+							version = "LuaJIT",
+						},
+						workspace = {
+							checkThirdParty = false,
+							library = {
+								vim.env.VIMRUNTIME,
+							},
+							-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+							-- library = vim.api.nvim_get_runtime_file("", true),
+						},
+					})
+				end,
+				settings = {
+					Lua = {
+						workspace = {
+							-- I'd love for this to use the `userThirdParty` key, but it doesn't seem to work
+							library = { vim.fn.stdpath("data") .. "/lazy/love2d" },
+						},
+					},
+				},
+			})
+
+			lspconfig.nushell.setup({})
+
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+			vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+			-- Use LspAttach autocommand to only map the following keys after the
+			-- language server attaches to the current buffer
+			local lsp_group = vim.api.nvim_create_augroup("UserLspConfig", {})
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = lsp_group,
+				callback = function(ev)
+					-- Enable completion triggered by <c-x><c-o>
+					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+					-- Buffer local mappings.
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+					local opts = { buffer = ev.buf }
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					-- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+					vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+					vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+					vim.keymap.set("n", "<leader>wl", function()
+						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+					end, opts)
+					vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					-- conform.nvim will handle formatting, falling back to the lsp
+					-- optionally
+					-- vim.keymap.set('n', '<leader>f', function()
+					--   vim.lsp.buf.format { async = true }
+					-- end, opts)
+				end,
+			})
+		end,
+	},
 }
