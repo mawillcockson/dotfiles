adcfe4ab695d64fce9ef16ec40e96ce56dcd3ba5
Matthew W <matthew@willcockson.family>
2024-03-28 02:58:49 -0500

    feat(nvim): make conjure lazy

--- dotfiles/nvim/lua/plugins/clojure.lua
+++ dotfiles/nvim/lua/plugins/clojure.lua
@@ -1,20 +1,31 @@
-vim.cmd[[:let g:conjure#highlight#enabled = v:true]]
+vim.cmd([[:let g:conjure#highlight#enabled = v:true]])
+
 return {
-  -- NOTE: I'd like to detect if scoop is installed and then install temurin-lts-jdk and clj-deps as required
-  -- It used to be adoptopenjdk-lts-hotspot, which at one point was referenced in the exercism docs:
-  -- https://github.com/exercism/clojure/blob/c1b58f4ed671a1f99d052201cde4fe19f62b36f6/docs/INSTALLATION.md
-  -- This was changed at some point in the java bucket:
-  -- https://github.com/ScoopInstaller/Java/issues/100#issuecomment-903529054
-  -- As well, the clojure manifest was superceded by clj-deps:
-  -- https://github.com/littleli/scoop-clojure/pull/194
-  -- The scoop-clojure repository has a great wiki explaining the current setup steps:
-  -- https://github.com/littleli/scoop-clojure/wiki/Getting-started
-  "Olical/conjure",
-  dependencies = {
-    -- NOTE: :Clj not working
--- https://github.com/Olical/conjure/wiki/Quick-start:-Clojure#start-your-nrepl--cider-middleware
-    "clojure-vim/vim-jack-in",
-    "tpope/vim-dispatch",
-    "radenling/vim-dispatch-neovim",
-  },
+	-- NOTE: I'd like to detect if scoop is installed and then install temurin-lts-jdk and clj-deps as required
+	-- It used to be adoptopenjdk-lts-hotspot, which at one point was referenced in the exercism docs:
+	-- https://github.com/exercism/clojure/blob/c1b58f4ed671a1f99d052201cde4fe19f62b36f6/docs/INSTALLATION.md
+	-- This was changed at some point in the java bucket:
+	-- https://github.com/ScoopInstaller/Java/issues/100#issuecomment-903529054
+	-- As well, the clojure manifest was superceded by clj-deps:
+	-- https://github.com/littleli/scoop-clojure/pull/194
+	-- The scoop-clojure repository has a great wiki explaining the current setup steps:
+	-- https://github.com/littleli/scoop-clojure/wiki/Getting-started
+	"Olical/conjure",
+	branch = "master",
+	version = "*",
+	lazy = true,
+	ft = { "fennel", "clojure" },
+	config = function(name, opts)
+		require("conjure.main").main()
+		require("conjure.mapping")["on-filetype"]()
+    require("cmp-conjure").setup()
+	end,
+	dependencies = {
+		"PaterJason/cmp-conjure",
+		-- NOTE: :Clj not working
+		-- https://github.com/Olical/conjure/wiki/Quick-start:-Clojure#start-your-nrepl--cider-middleware
+		"clojure-vim/vim-jack-in",
+		"tpope/vim-dispatch",
+		"radenling/vim-dispatch-neovim",
+	},
 }
---
+++ dotfiles/nvim/lua/plugins/cmp-conjure.lua
@@ -0,0 +1,19 @@
+return {
+	"PaterJason/cmp-conjure",
+  branch = "master",
+  version = false,
+  lazy = true, -- loaded by require where needed
+	config = function(name, opts)
+		local cmp = require("cmp")
+		local config = cmp.get_config()
+		table.insert(config.sources, {
+			name = "buffer",
+			option = {
+				sources = {
+					{ name = "conjure" },
+				},
+			},
+		})
+		cmp.setup(config)
+	end,
+}
