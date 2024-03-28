7dcbc72fdd0f2f225c049a2c50b683f54bc9ddeb
Matthew W <matthew@willcockson.family>
2024-03-28 03:32:50 -0500

    feat(nvim): set nvim-tree to do what I've been wanting this whole time!

--- dotfiles/nvim/lua/plugins/nvim-tree.lua
+++ dotfiles/nvim/lua/plugins/nvim-tree.lua
@@ -1,13 +1,45 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  branch = "master",
-  version = "*",
-  lazy = true,
-  keys = {
-    { "<leader>fb", "<cmd>NvimTreeToggle<cr>", desc = "NvimTree" },
-  },
-  config = true,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
+	"nvim-tree/nvim-tree.lua",
+	branch = "master",
+	version = "*",
+	lazy = true,
+	keys = {
+		{ "<leader>fb", "<cmd>NvimTreeToggle<cr>", desc = "NvimTree" },
+	},
+	opts = {
+    actions = {
+      change_dir = {
+        enable = true,
+        -- runs :cd when the root is changed
+        global = true,
+      },
+    },
+    -- updates the nvim-tree folder browser when :cd is run
+    sync_root_with_cwd = false,
+		-- custom keymappings
+		on_attach = function(bufnr)
+			local nvim_tree_api = require("nvim-tree.api")
+			-- configure default mappings
+			nvim_tree_api.config.mappings.default_on_attach(bufnr)
+
+			local function opts(desc)
+				return {
+					["desc"] = "nvim-tree: " .. tostring(desc),
+					buffer = bufnr,
+					noremap = true,
+					silent = true,
+					nowait = true,
+				}
+			end
+
+      --[[
+			vim.keymap.set("n", "-", function()
+				nvim_tree_api.tree.change_root_to_parent()
+			end, opts("Go up a directory"))
+      --]]
+		end,
+	},
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
 }
