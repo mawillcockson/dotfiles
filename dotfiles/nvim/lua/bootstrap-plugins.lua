ed5acb97ce73cdd53dd36b05e85a1339e6bd6a18
Matthew W <matthew@willcockson.family>
2024-04-24 23:23:18 -0500

    fix(nvim): bootstrap with nvim --clean -l bootstrap-plugins.lua

--- dotfiles/nvim/lua/bootstrap-plugins.lua
+++ dotfiles/nvim/lua/bootstrap-plugins.lua
@@ -1,3 +1,6 @@
+-- This file is meant to be run like `nvim -l bootstrap-plugins.lua`, a feature added in nvim 0.9x
+--
+-- NOTE::OUTDATED
 -- This file is not automatically read at startup. Instead, an autocommand is
 -- setup to watch for changes to plugins/plugin-spec.lua and do the appropriate
 -- things in response to that.
@@ -7,62 +10,61 @@
 local ERROR = vim.log.levels.ERROR
 local WARN = vim.log.levels.WARN
 local INFO = vim.log.levels.INFO
+local DEBUG = vim.log.levels.DEBUG
 
-if not vim.fn.executable "git" then
-  vim.notify("git required for lazy.nvim package manager", ERROR, {})
-  return
+if not vim.fn.executable("git") then
+	vim.notify("git required for lazy.nvim package manager", ERROR, {})
+  os.exit(1)
 end
-
 
 -- DONE: Need to switch to lazy.nvim
 -- https://github.com/folke/lazy.nvim#-installation
 vim.notify("importing join_path", DEBUG, {})
 local join_path = require("utils").join_path
 local lazypath = join_path(vim.fn.stdpath("data"), "lazy", "lazy.nvim")
-vim.notify(
-  "lazy.nvim installed to (lazypath): "..tostring(lazypath),
-  DEBUG, {}
-)
+vim.notify("lazy.nvim installed to (lazypath): " .. tostring(lazypath), DEBUG, {})
 local did_bootstrap = false
 if not vim.loop.fs_stat(lazypath) then
-  vim.notify("bootstrapping lazy.nvim to: "..lazypath, INFO, {})
-  did_bootstrap = vim.fn.system({
-    "git",
-    "clone",
-    "--depth=1",
-    "--filter=blob:none",
-    "--single-branch",
-    -- This uses a trick: there's no branch named stable, but there is a tag
-    -- named stable that is constantly updated to point to the latest stable
-    -- commit on master
-    "--branch=stable", -- latest stable release
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
+	vim.notify("bootstrapping lazy.nvim to: " .. lazypath, INFO, {})
+	did_bootstrap = vim.fn.system({
+		"git",
+		"clone",
+		"--depth=1",
+		"--filter=blob:none",
+		"--single-branch",
+		-- This uses a trick: there's no branch named stable, but there is a tag
+		-- named stable that is constantly updated to point to the latest stable
+		-- commit on master
+		"--branch=stable", -- latest stable release
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
 end
 
 -- plain substring search, so I don't have to worry about escaping `lazypath`
 -- https://www.lua.org/manual/5.1/manual.html#pdf-string.find
 if not vim.o.rtp:find(lazypath, 1, true) then
-  vim.notify("adding lazypath to rtp", DEBUG, {})
-  vim.opt.rtp:prepend(lazypath)
+	vim.notify("adding lazypath to rtp", DEBUG, {})
+	vim.opt.rtp:prepend(lazypath)
 end
 
 vim.notify("loading lazy.nvim", INFO, {})
 local ok, lazy = pcall(require, "lazy")
 if not ok then
-  vim.notify("lazy.nvim not installed, cannot manage plugins", ERROR, {})
-  return
+	vim.notify("lazy.nvim not installed, cannot manage plugins", ERROR, {})
+	return
 end
 
 -- in case they're needed elsewhere
 local opts = require("lazy_opts")
-local spec = opts.spec
+--local spec = opts.spec
 vim.notify("running lazy.nvim setup", DEBUG, {})
 local original = vim.go.loadplugins
 -- NOTE: This feels wrong, and I'm curious why I need it when I run the environment under
 -- nvim --headless -u NONE -i NONE -S $this_script "+q"
-vim.go.loadplugins = true
+-- Perhaps because `:help --clean` describes that -i NONE -u NONE doesn't load
+-- builtin plugins?
+--vim.go.loadplugins = true
 vim.notify("before lazy.setup(), vim.go.loadplugins = " .. tostring(vim.go.loadplugins) .. "", DEBUG, {})
 lazy.setup(opts)
 -- lazy.load{spec}
@@ -71,11 +73,21 @@ vim.notify("finished lazy.setup()", DEBUG, {})
 vim.go.loadplugins = original
 
 -- NOTE: I'd like to run lazy.restore() if a lock file is present, and
--- lazy.sync() otherwise
+-- lazy.install() otherwise
 if vim.g.lazy_install_plugins then
-  vim.notify("(lazy.nvim) installing only new plugins; use :Lazy to update existing ones as well", INFO, {})
-  lazy.install{ wait = true }
+  local lockfile = join_path(vim.fn.stdpath("config"), "lazy-lock.json")
+  if vim.loop.fs_stat(lockfile) then
+    vim.notify("(lazy.nvim) restoring plugins as described in lockfile -> " .. tostring(lockfile), INFO, {})
+  lazy.restore({wait = true})
+  else
+    vim.notify("(lazy.nvim) installing only new plugins; use :Lazy to update existing ones as well", INFO, {})
+    lazy.install({ wait = true })
+  end
 elseif did_bootstrap then
-  vim.notify("lazy.nvim was just bootstrapped, but vim.g.lazy_install_plugins wasn't"..
-  " set to true, so :Lazy should be run once the editor is running", WARN, {})
+	vim.notify(
+		"lazy.nvim was just bootstrapped, but vim.g.lazy_install_plugins wasn't"
+			.. " set to true, so :Lazy should be run once the editor is running",
+		WARN,
+		{}
+	)
 end
