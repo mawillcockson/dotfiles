1d8f0b2c4662fbbe4d9d351b0e43acad8b6e8374
Matthew W <matthew@willcockson.family>
2024-05-23 06:11:58 -0500

    refactor(nvim/wk): convert nvim-tree

--- dotfiles/nvim/lua/plugins/nvim-tree.lua
+++ dotfiles/nvim/lua/plugins/nvim-tree.lua
@@ -7,36 +7,32 @@ return {
 		{ "<leader>fb", "<cmd>NvimTreeToggle<cr>", desc = "NvimTree" },
 	},
 	opts = {
-    actions = {
-      change_dir = {
-        enable = true,
-        -- runs :cd when the root is changed
-        global = true,
-      },
-    },
-    -- updates the nvim-tree folder browser when :cd is run
-    sync_root_with_cwd = false,
+		actions = {
+			change_dir = {
+				enable = true,
+				-- runs :cd when the root is changed
+				global = true,
+			},
+		},
+		-- updates the nvim-tree folder browser when :cd is run
+		sync_root_with_cwd = false,
 		-- custom keymappings
 		on_attach = function(bufnr)
 			local nvim_tree_api = require("nvim-tree.api")
 			-- configure default mappings
 			nvim_tree_api.config.mappings.default_on_attach(bufnr)
 
-			local function opts(desc)
-				return {
-					["desc"] = "nvim-tree: " .. tostring(desc),
-					buffer = bufnr,
-					noremap = true,
-					silent = true,
-					nowait = true,
-				}
-			end
-
-      --[[
-			vim.keymap.set("n", "-", function()
-				nvim_tree_api.tree.change_root_to_parent()
-			end, opts("Go up a directory"))
-      --]]
+			--[[
+			require("which-key").register(
+				{ ["-"] = {
+					function()
+						nvim_tree_api.tree.change_root_to_parent()
+					end,
+					"Go up a directory",
+				} },
+				{ buffer = bufnr, noremap = true, silent = true, nowait = true }
+			)
+			--]]
 		end,
 	},
 	dependencies = {
