0cbddbd6796089a6f1f57d4ac1ab9e9809166edf
Matthew W <matthew@willcockson.family>
2024-06-08 02:17:05 -0500

    feat(clink/starship): add assert os.setenv() exits properly and remove P28F

--- dotfiles/clink/starship.lua
+++ dotfiles/clink/starship.lua
@@ -17,17 +17,18 @@ local starship_dir = (
 local starship_installed = io.popen("starship --version", "r"):close()
 
 local function starship_init ()
-	os.setenv("STARSHIP_CONFIG", starship_dir .. "\\starship.toml")
-	os.setenv("STARSHIP_CACHE", starship_dir)
+	assert(os.setenv("STARSHIP_CONFIG", starship_dir .. "\\starship.toml"))
+	assert(os.setenv("STARSHIP_CACHE", starship_dir))
 	-- os.setenv("STARSHIP_LOG", "trace")
-	local starship_init_cmd = io.popen("starship init cmd")
-	local starship_init_cmd_func, loading_err = load(starship_init_cmd:read("*a"))
-	starship_init_cmd:close()
-	if starship_init_cmd_func then starship_init_cmd_func() end
+	local init_cmd = assert(io.popen("starship init cmd"))
+	local init_cmd_func, loading_err = load(init_cmd:read("*a"))
+	init_cmd:close()
+	if init_cmd_func then init_cmd_func() end
 	return loading_err
 end
 
-if os.getenv("COMPUTERNAME") ~= "P28F" and starship_installed then
+local starship_loading_err
+if --[[os.getenv("COMPUTERNAME") ~= "P28F" and]] starship_installed then
 	starship_loading_err = starship_init()
 end
 
@@ -39,7 +40,7 @@ if lua_debug then
 	print("starship: " .. (
 		starship_loading_err and "[failed]" or "[loaded]"
 	))
-	if starship_loading_err then 
+	if starship_loading_err then
 		print("error loading starship: " .. tostring(starship_loading_err))
 		pause()
 	end
