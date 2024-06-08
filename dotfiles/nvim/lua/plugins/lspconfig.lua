9f578b3924d39661c3258d632abdb8faf9e11598
Matthew W <matthew@willcockson.family>
2024-06-08 02:19:46 -0500

    feat(nvim/mason): use max_nproc

--- dotfiles/nvim/lua/plugins/lspconfig.lua
+++ dotfiles/nvim/lua/plugins/lspconfig.lua
@@ -68,7 +68,7 @@ return {
 		-- lazy overrules the priority, so the priority has no meaning to lazy
 		lazy = true,
 		priority = 100, -- https://github.com/williamboman/mason-lspconfig.nvim/tree/v1.27.0#setup
-		config = true,
+		opts = { max_concurrent_installers = require("utils").calculate_nproc() or vim.g.max_nproc_default or 1 },
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
