fc6076ab9d29ee730f23183eb863328775c4f7ff
Matthew W <matthew@willcockson.family>
2024-06-08 19:24:10 -0500

    style(nvim/uis): move log call to top of file

--- dotfiles/nvim/lua/local/uis/lua/uis/tui_windows_terminal.lua
+++ dotfiles/nvim/lua/local/uis/lua/uis/tui_windows_terminal.lua
@@ -1,3 +1,4 @@
+vim.notify("setting up for running under Windows Terminal", vim.log.levels.DEBUG)
+
 -- NOTE::IMPROVEMENT it would be nice if the font-switching function could be
 -- overridden to change the font on the fly in this specific tab of this window
-vim.notify("setting up for running under Windows Terminal", vim.log.levels.DEBUG)
