cedb02590d293dcc2e375e9c5d6a8cdd9910fd63
Matthew W <matthew@willcockson.family>
2024-07-20 01:23:02 -0500

    refactor(chezmoi): rearrange files

--- dotfiles/clink/clink_start.cmd
+++
@@ -1,7 +0,0 @@
-if not defined FNM_AUTORUN_GUARD (
-    where fnm >nul 2>nul
-    if NOT ERRORLEVEL 1 (
-          set "FNM_AUTORUN_GUARD=AutorunGuard"
-          FOR /f "tokens=*" %%z IN ('fnm env --use-on-cd --version-file-strategy=recursive --shell=cmd') DO CALL %%z
-    ) else set "ERRORLEVEL=0"
-)
