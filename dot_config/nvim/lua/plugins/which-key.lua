3d5ce6080d8b683d3ab35b01989f0c80f3a3a296
Matthew W <matthew@willcockson.family>
2024-08-14 15:23:39 -0500

    fix(nvim/wk): use tab switching hotkeys from browser

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -122,7 +122,9 @@ return {
 			},
 
 			{ "<C-l>", "<Cmd>:tabnext<CR>", desc = "switch tab rightwards", mode = { "n", "i", "t" } },
+			{ "<C-PageDown>", "<Cmd>:tabnext<CR>", desc = "switch tab rightwards", mode = { "n", "i", "t" } },
 			{ "<C-h>", "<Cmd>:tabprevious<CR>", desc = "switch tab leftwards", mode = { "n", "i", "t" } },
+			{ "<C-PageUp>", "<Cmd>:tabprevious<CR>", desc = "switch tab leftwards", mode = { "n", "i", "t" } },
       {"<leader>m", group = "miscellaneous"},
 			{
 				"<leader>mi",
