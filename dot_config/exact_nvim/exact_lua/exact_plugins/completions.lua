a6e155b99ae0a42c2b15c4cd12abcdc2f2c3fde3
Matthew W <matthew@willcockson.family>
2024-11-17 06:02:25 -0600

    fix(nvim/completions): re-enable sections

--- dot_config/exact_nvim/exact_lua/exact_plugins/completions.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/completions.lua
@@ -58,25 +58,23 @@ return {
 			--
 
 			-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-			--[==[ something ate my ram, so disabling these in the hopes it goes away
-		cmp.setup.cmdline({ "/", "?" }, {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = {
-				{ name = "buffer" },
-			},
-		})
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
 
-		-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-		cmp.setup.cmdline(":", {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = cmp.config.sources({
-				{ name = "path" },
-			}, {
-				{ name = "cmdline" },
-			}),
-			matching = { disallow_symbol_nonprefix_matching = false },
-		})
-		--]==]
+			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+				matching = { disallow_symbol_nonprefix_matching = false },
+			})
 		end,
 	},
 
