82455c0cba6f120075c0ef1a39ca88f7341d2242
Matthew W <matthew@willcockson.family>
2025-02-25 21:40:09 -0600

    feat(nvim): add telescope.nvim

--- dot_config/exact_nvim/exact_lua/exact_plugins/telescope-undo.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/telescope-undo.lua
@@ -13,7 +13,6 @@ return {
 	dependencies = { -- note how they're inverted to above example
 		{
 			"nvim-telescope/telescope.nvim",
-			dependencies = { "nvim-lua/plenary.nvim" },
 		},
 	},
 	opts = {
---
+++ dot_config/exact_nvim/exact_lua/exact_plugins/telescope.lua
@@ -0,0 +1,59 @@
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		lazy = true,
+		--event = "VeryLazy", -- key mappings do this for now
+		cmd = {
+			"Telescope",
+		},
+		version = "*",
+		opts = {
+			extensions = {
+				fzf = {},
+			},
+		},
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
+		keys = {
+			{ "<leader>ff", "<Cmd>Telescope find_files<CR>", desc = "Telescope find files" },
+			{ "<leader>fg", "<Cmd>Telescope live_grep<CR>", desc = "Telescope live grep" },
+			{ "<leader>fb", "<Cmd>Telescope buffers<CR>", desc = "Telescope buffers" },
+			{ "<leader>fh", "<Cmd>Telescope help_tags<CR>", desc = "Telescope help tags" },
+		},
+		config = function(_, opts)
+			local telescope = require("telescope")
+			telescope.setup(vim.tbl_deep_extend("force", opts, {}))
+
+			-- To get fzf loaded and working with telescope, you need to call
+			-- load_extension, somewhere after setup function:
+			telescope.load_extension("fzf")
+
+			local builtin = require("telescope.builtin")
+			local wk = require("which-key")
+			wk.add({
+				{ "<leader>ff", builtin.find_files, desc = "Telescope find files" },
+				{ "<leader>fg", builtin.live_grep, desc = "Telescope live grep" },
+				{ "<leader>fb", builtin.buffers, desc = "Telescope buffers" },
+				{ "<leader>fh", builtin.help_tags, desc = "Telescope help tags" },
+			})
+		end,
+	},
+
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		lazy = true,
+		build = function(plugin_spec)
+			local result = vim.system(
+				{ "nu", "-c", [[cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release; cmake --build build --config Release]] },
+				{
+					cwd = plugin_spec.dir,
+					text = true,
+				}
+			):wait()
+			if result.code ~= 0 then
+				coroutine.yield(result.stderr, vim.log.levels.TRACE)
+				return error("failed to build telescope-fzf-native")
+			end
+			coroutine.yield(result.stdout, vim.log.levels.TRACE)
+		end,
+	},
+}
