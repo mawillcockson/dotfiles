dbc8431951c3e600a423446564db3b013e9a703d
Matthew W <matthew@willcockson.family>
2025-07-10 15:22:41 -0500

    style(nvim/tree-sitter): apply stylua

--- dot_config/exact_nvim/exact_lua/exact_plugins/treesitter.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/treesitter.lua
@@ -4,25 +4,25 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter/blob/v0.9.2/lua/nvim-treesitter/install.lua#L19
 -- https://github.com/nvim-treesitter/nvim-treesitter/blob/v0.9.2/lua/nvim-treesitter/shell_command_selectors.lua#L74-L80
 
-local default_compilers = { vim.fn.getenv "CC", "cc", "gcc", "clang", "cl", "zig" }
+local default_compilers = { vim.fn.getenv("CC"), "cc", "gcc", "clang", "cl", "zig" }
 
 ---@return bool|nil
 local has_compiler = vim.tbl_filter(function(c) ---@param c string
-  return c ~= vim.NIL and vim.fn.executable(c) == 1
+	return c ~= vim.NIL and vim.fn.executable(c) == 1
 end, default_compilers)[1]
 
-local has_tree_sitter = vim.fn.executable "tree-sitter" == 1
+local has_tree_sitter = vim.fn.executable("tree-sitter") == 1
 
 vim.notify(
-  "has_compiler -> " .. tostring(has_compiler) .. "\n" .. "has tree-sitter -> " .. tostring(has_tree_sitter),
-  vim.log.levels.DEBUG,
-  {}
+	"has_compiler -> " .. tostring(has_compiler) .. "\n" .. "has tree-sitter -> " .. tostring(has_tree_sitter),
+	vim.log.levels.DEBUG,
+	{}
 )
-vim.notify("$PATH -> " .. os.getenv "PATH", vim.log.levels.DEBUG)
+vim.notify("$PATH -> " .. os.getenv("PATH"), vim.log.levels.DEBUG)
 
 local enable = has_compiler and has_tree_sitter
 if not enable then
-  vim.notify("disabling tree-sitter since compiler and tree-sitter-cli aren't both present", vim.log.levels.WARN)
+	vim.notify("disabling tree-sitter since compiler and tree-sitter-cli aren't both present", vim.log.levels.WARN)
 end
 
 -- NOTE: so far, the MSVC compiler hasn't been working, while the zig compiler
