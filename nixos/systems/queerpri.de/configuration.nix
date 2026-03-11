f39295530c825d36cb5ed03e3d83e060562c54c5
Matthew W <matthew@willcockson.family>
2026-03-11 03:19:05 -0500

    feat(nixos/queerpri.de): allow passwordless sudo in testing

--- nixos/systems/queerpri.de/configuration.nix
+++ nixos/systems/queerpri.de/configuration.nix
@@ -9,6 +9,9 @@
       cores = 1;
       graphics = false;
     };
+    # Whether users of the wheel group must provide a password to run commands
+    # as super user via sudo.
+    security.sudo.wheelNeedsPassword = false;
   };
 in {
   imports = [
@@ -17,6 +20,12 @@ in {
   ];
 
   #boot.sys
+
+  # Only allow members of the wheel group to execute sudo by setting the
+  # executable’s permissions accordingly. This prevents users that are not
+  # members of wheel from exploiting vulnerabilities in sudo such as
+  # CVE-2021-3156.
+  security.sudo.execWheelOnly = true;
 
   networking.hostName = "queerpri";
   networking.domain = "de";
