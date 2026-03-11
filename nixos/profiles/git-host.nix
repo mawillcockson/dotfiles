5e4f03cdc897cbc0c3102cf05ddb1e538235c9d9
Matthew W <matthew@willcockson.family>
2026-03-11 04:29:00 -0500

    docs(nixos/git-host): add why this configuration exists

--- nixos/profiles/git-host.nix
+++ nixos/profiles/git-host.nix
@@ -20,6 +20,7 @@ in {
     # NOTE:DONE add the shell to /etc/shells
     # NOTE: and add a symlink at /usr/bin/git-shell
     shell = git-shell;
+    # this should disable password login for this account
     initialHashedPassword = null;
   };
   # NOTE: create custom authorized_keys file and authorized_principals files, specifically for this configuration module, and add those files to the general sshd config
