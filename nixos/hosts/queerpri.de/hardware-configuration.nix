8e3cbd3ed1d60f41c8cbdeb9963be45447fbc6d6
Matthew W <matthew@willcockson.family>
2026-03-20 06:07:00 -0500

    fix(nixos/queerpri.de): don't set `system`, as it's set elsewhere
    If this really shouldn't be set, then I could change the
    queepri.de/configuration.nix to not import ./hardware-configuration.nix
    itself, and instead import that where it's needed, and optionally remove
    the attributes that are conflicting, like `nixpkgs.hostPlatform`.

--- nixos/hosts/queerpri.de/hardware-configuration.nix
+++ nixos/hosts/queerpri.de/hardware-configuration.nix
@@ -35,6 +35,6 @@
     #{device = "/dev/disk/by-partlabel/swap";}
   ];
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  #nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
