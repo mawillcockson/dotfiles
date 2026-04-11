9de545f71dc192f7b8ceab5e2b03627159e9d9aa
Matthew W <matthew@willcockson.family>
2026-04-11 12:38:21 -0500

    tests(nixos/mw-pki): have intermediateCA retrieve a cert

--- nixos/profiles/mw-pki/tests/default.nix
+++ nixos/profiles/mw-pki/tests/default.nix
@@ -4,49 +4,41 @@
   pkgs,
   ...
 }: let
-  myLaptop = {...}: {
+  rootCA = {...}: {
     virtualisation.vlans = [1];
     services.mw-pki.rootCA = {
       enable = true;
       insecure = true;
     };
-    imports = [self.nixosModules.mw-pki];
+    imports = [../root-ca.nix];
   };
   # make it clear how I want to run this
-  rootCA = myLaptop;
+  myLaptop = rootCA;
   intermediateCA = {...}: {
     virtualisation.vlans = [
       1
       2
     ];
-    services.mw-pki.intermediateCA.enable = true;
-    imports = [self.nixosModules.mw-pki];
+    #imports = [self.nixosModules.mw-pki];
+    #services.mw-pki.intermediateCA.enable = true;
+    environment.systemPackages = [
+      pkgs.step-ca
+      pkgs.step-cli
+    ];
   };
-  server = {...}: {
-    virtualisation.vlans = [2];
-    services.mw-pki.server.enable = true;
-    imports = [self.nixosModules.mw-pki];
-  };
-  client = {...}: {
-    virtualisation.vlans = [2];
-    services.mw-pki.client.enable = true;
-    imports = [self.nixosModules.mw-pki];
-  };
+  #server = {...}: {
+  #  virtualisation.vlans = [2];
+  #  services.mw-pki.server.enable = true;
+  #  imports = [self.nixosModules.mw-pki];
+  #};
+  #client = {...}: {
+  #  virtualisation.vlans = [2];
+  #  services.mw-pki.client.enable = true;
+  #  imports = [self.nixosModules.mw-pki];
+  #};
 in
   pkgs.testers.runNixOSTest {
     imports = [
-      {
-        name = "mw-pki tests";
-        nodes = {inherit rootCA;};
-        testScript =
-          /*
-          python
-          */
-          ''
-            myLaptop.start(allow_reboot=True)
-            myLaptop.wait_for_unit("ssh-ca.service")
-          '';
-      }
       {
         name = "mw-pki: intermediate CA";
         nodes = {inherit myLaptop intermediateCA;};
@@ -55,40 +47,42 @@ in
           python
           */
           ''
-            intermediateCA.start(allow_reboot=True)
-            intermediateCA.wait_for_unit("ssh-ca.service")
+            intermediateCA.start()
+            myLaptop.wait_for_unit("mw-pki-rootCA.service")
+            intermediateCA.succeed(
+                "sudo -u test -- step 
           '';
       }
 
-      (lib.makeScope pkgs.newScope (self'': {
-        name = "mw-pki: client / full";
-        nodes = {
-          inherit
-            myLaptop
-            intermediateCA
-            server
-            client
-            ;
-        };
-        testScript = let
-          vlan = builtins.elemAt server.virtualisation.vlans 0;
-          machinePos = builtins.unsafeGetAttrPos "server" self''.nodes;
-          serverIP = "192.168.${vlan}.${machinePos}";
-        in
-          /*
-          python
-          */
-          ''
-            start_all()
-            myLaptop.wait_for_unit("ssh-ca.service")
-            intermediateCA.wait_for_unit("ssh-ca.service")
-            server.wait_for_unit("ssh-ca.service")
-            client.wait_for_unit("ssh-ca.service")
-            # NOTE::IMPROVEMENT is there a way to pull the IP address in from
-            # whichever part of nixpkgs processes the `virtualisation.vlans` option?
-            client.succeed("ssh ${serverIP} -- echo 'works!'")
-          '';
-      }))
+      #(lib.makeScope pkgs.newScope (self'': {
+      #  name = "mw-pki: client / full";
+      #  nodes = {
+      #    inherit
+      #      myLaptop
+      #      intermediateCA
+      #      #server
+      #      #client
+      #      ;
+      #  };
+      #  testScript = let
+      #    vlan = builtins.elemAt server.virtualisation.vlans 0;
+      #    machinePos = builtins.unsafeGetAttrPos "server" self''.nodes;
+      #    serverIP = "192.168.${vlan}.${machinePos}";
+      #  in
+      #    /*
+      #    python
+      #    */
+      #    ''
+      #      start_all()
+      #      myLaptop.wait_for_unit("ssh-ca.service")
+      #      intermediateCA.wait_for_unit("ssh-ca.service")
+      #      server.wait_for_unit("ssh-ca.service")
+      #      client.wait_for_unit("ssh-ca.service")
+      #      # NOTE::IMPROVEMENT is there a way to pull the IP address in from
+      #      # whichever part of nixpkgs processes the `virtualisation.vlans` option?
+      #      client.succeed("ssh server -- echo 'works!'")
+      #    '';
+      #}))
     ];
     defaults = {
       documentation.enable = false;
