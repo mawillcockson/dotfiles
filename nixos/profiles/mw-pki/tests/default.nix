b0ba3432df241e59c91ad531dbface9b4136a194
Matthew W <matthew@willcockson.family>
2026-04-12 13:11:42 -0500

    tests(nixos/mw-pki): begin looking complete

--- nixos/profiles/mw-pki/tests/default.nix
+++ nixos/profiles/mw-pki/tests/default.nix
@@ -4,13 +4,34 @@
   pkgs,
   ...
 }: let
+rootCAKeyPasswordPath = "root-password-file";
+rootCAKeyPath = "root_ca.key";
+rootCACertPath = "root_ca.crt";
+    test-certificates = pkgs.runCommandLocal "test-certificates" { } ''
+      mkdir -p "$out"
+      printf '%s' 'insecure-root-password' > "$out/${rootCAKeyPasswordPath}"
+      ${lib.getExe pkgs.step-cli} certificate create \
+          "Example Root CA" \
+          "$out/${rootCACertPath}" \
+          "$out/${rootCAKeyPath}" \
+          --kty=OKP \
+          --profile=root-ca \
+          --password-file="$out/${rootCAKeyPasswordPath}" \
+          --not-before=-10m \
+          --not-after="24h"
+      # ${lib.getExe pkgs.step-cli} certificate create "Example Intermediate CA 1" $out/intermediate_ca.crt $out/intermediate_ca.key --password-file=$out/intermediate-password-file --ca-password-file=$out/root-password-file --profile intermediate-ca --ca $out/root_ca.crt --ca-key $out/root_ca.key
+    '';
   rootCA = {...}: {
     virtualisation.vlans = [1];
-    services.mw-pki.rootCA = {
+    services.mw-pki.intermediateCA = {
       enable = true;
-      insecure = true;
+      beRootCA = true;
+      rootCAKeyPasswordPath = "${test-certificates}/${rootCAKeyPasswordPath}";
+      rootCAKeyPath = "${test-certificates}/${rootCAKeyPath}";
+      rootCACertPath = "${test-certificates}/${rootCACertPath}";
     };
-    imports = [../root-ca.nix];
+    services.step-ca.settings.dnsNames = ["myLaptop"];
+    imports = [self.nixosModules.mw-pki];
   };
   # make it clear how I want to run this
   myLaptop = rootCA;
@@ -19,8 +40,8 @@
       1
       2
     ];
-    #imports = [self.nixosModules.mw-pki];
-    #services.mw-pki.intermediateCA.enable = true;
+    imports = [self.nixosModules.mw-pki];
+    services.mw-pki.intermediateCA.enable = true;
     environment.systemPackages = [
       pkgs.step-ca
       pkgs.step-cli
@@ -50,7 +71,20 @@ in
             intermediateCA.start()
             myLaptop.wait_for_unit("mw-pki-rootCA.service")
             intermediateCA.succeed(
-                "sudo -u test -- step 
+                "sudo -u test -- step ca bootstrap --ca-url=http://myLaptop:${
+              (lib.nixosSystem {
+                modules = [self.nixosModules.mw-pki.rootCA];
+                inherit (pkgs.stdenv.hostPlatform) system;
+              }).config.services.step-ca.port
+            } --fingerprint=")
+            intermediateCA.succeed("check that myLaptop's key is useable")
+            myLaptop.shutdown()
+            intermediateCA.shutdown()
+
+            # do it again to simulate the computers being restarted
+            intermediateCA.start()
+            myLaptop.wait_for_unit("mw-pki-rootCA.service")
+            intermediateCA.succeed("check that myLaptop's key is useable")
           '';
       }
 
