6f9dfc6e8bb92891ab5c6e6eb09b3ac40ce04b63
Matthew W <matthew@willcockson.family>
2026-06-10 03:11:40 -0500

    fix(sh): don't print during non-interactive startup

--- dot_profile.d/51_pyenv.sh
+++ dot_profile.d/51_pyenv.sh
@@ -1,10 +1,16 @@
 if ! command -v pyenv >/dev/null 2>&1; then
     PATH="${PATH}:${PYENV_ROOT:-"${HOME}/.pyenv"}/bin"
 fi
-if command -v pyenv; then
+if command -v pyenv >/dev/null 2>&1; then
     if ! eval "$(pyenv init --path)"; then
         printf 'problem initializing pyenv\n'
     fi
 else
-    printf 'cannot find pyenv!\n'
+    case "${-:-}" in
+        *i*)
+            printf 'cannot find pyenv!\n'
+            ;;
+        *)
+            ;;
+    esac
 fi
